40d9fe2a3aeae6b5924ff6a144886095c031712b
AyoubCherguelaine <cherguelainea@gmail.com>
2023-05-21 16:03:50 +0100

    final

---
+++ main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from apis import doc_classifier, language, transformation, config
+import shutil
+import os
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+class TextInput(BaseModel):
+    text: str
+
+
+@app.post("/api/classifier")
+async def classify_text(text: TextInput):
+    out = doc_classifier.pred(text.text)
+    if out is not None:
+        return out
+    else:
+        return {"Problem": "no solution!"}
+
+
+@app.post("/api/language")
+async def detect_language(text: TextInput):
+    out = language.pred(text.text)
+    if out is not None:
+        return out
+    else:
+        return {"Problem": "no solution!"}
+
+
+@app.post("/api/transformer")
+async def transform_file(file: UploadFile = File(...)):
+    file_path = f"static/{file.filename}"
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    text = transformation.extract_text(file.filename, file_path)
+    return {"filename": file.filename, "content": text}
+
+
+@app.post("/classify")
+async def classify_uploaded_file(file: UploadFile = File(...)):
+    file_path = f"static/{file.filename}"
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    text = transformation.extract_text(file.filename, file_path)
+    lang = language.pred(text)
+    if lang is not None:
+        if lang["label"] == "en":
+            topic = doc_classifier.pred(text)
+            if topic is not None:
+                result = {
+                    "label": topic["labels"][0],
+                    "score": topic["scores"][0],
+                    "language": "en"
+                }
+            else:
+                result = {"exception": 4, "type": "classifier"}
+        else:
+            result = {
+                "exception": 3,
+                "type": "not english",
+                "language": lang["label"]
+            }
+    else:
+        result = {"exception": 2, "type": "language detection"}
+    os.remove(file_path)
+    return result
+
+
+@app.post("/configlabel")
+async def configure_labels(text: TextInput):
+    cleaned_str = text.text.strip('[]').replace(' ', '')
+    result_list = cleaned_str.split(',')
+    config.labels = result_list
+
+
+@app.get("/labels")
+async def get_labels():
+    return config.labels
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,  port=4002)
